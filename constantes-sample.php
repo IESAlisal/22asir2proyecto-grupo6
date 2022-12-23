370946f9f4ae8a876d3cbebf82d64e3b384b575b
Javier Terán González <javiteran@hotmail.com>
2022-12-23 10:24:51 +0100

    Subir código del grupo 4

---
+++ constantes-sample.php
@@ -0,0 +1,7 @@
+$servername = "localhost";
+$database = "libros2";
+$username = "usuario";
+$password = "usuariopass";
+
+$instance_id = file_get_contents("http://169.254.169.254/latest/meta-data/instance-id");
+echo "<h1>$instance_id</h1>";
---
+++ validaciones/conexion_db.php
@@ -0,0 +1,10 @@
+<?php
+    include '../constantes.php';
+
+    // Create connection
+    $conn = mysqli_connect($servername, $username, $password, $database);
+
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+?>
