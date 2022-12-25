8704dfb10afb3e61e952c12ef6a7cfbf4f97d44c
Javier Terán González <javiteran@hotmail.com>
2022-12-25 10:51:57 +0100

    Update constantes-sample.php

--- constantes-sample.php
+++ constantes-sample.php
@@ -4,5 +4,10 @@ $database = "libros2";
 $username = "usuario";
 $password = "usuariopass";
 
+define("HOST", "localhost");
+define("DATABASE", "libros2");
+define("USERNAME", "usuario");
+define("PASSWORD", "usuariopass");
+
 $instance_id = file_get_contents("http://169.254.169.254/latest/meta-data/instance-id");
 echo "<h1>$instance_id</h1>";
